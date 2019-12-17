be57f2a9f771343644e24c2970335eb727762dfd
lui <luisdecelis3@gmail.com>
2019-12-17 14:22:23 +0100

    feat: mergin with develop

--- src/components/Censo/style.ts
+++ src/components/Censo/style.ts
@@ -6,17 +6,11 @@ const shadowList = 'rgba(0, 0, 0, 0.1)'
 
 const styles = StyleSheet.create({
   container: { flex: 1, flexDirection: 'column', alignItems: 'center' },
-<<<<<<< HEAD
   descriptionItem: { color: textColor },
   image: {
     height: 25,
     width: 25
   },
-=======
-
-  descriptionItem: { color: textColor },
-
->>>>>>> 620af07844c2b9366d9ea80afb831befc252bc8b
   listItem: {
     alignSelf: 'center',
     borderRadius: 10,
@@ -28,24 +22,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.8,
     width: '85%'
   },
-<<<<<<< HEAD
   spaceList: {
     flex: 24
   },
-=======
-
-  palito: {
-    color: textColor,
-    fontFamily: 'sans-serif-light',
-    fontSize: 82,
-    marginLeft: '10%'
-  },
-
-  spaceList: {
-    flex: 24
-  },
-
->>>>>>> 620af07844c2b9366d9ea80afb831befc252bc8b
   title: {
     alignSelf: 'flex-start',
     color: textColor,
@@ -55,13 +34,7 @@ const styles = StyleSheet.create({
     marginRight: '5%',
     marginTop: '7%'
   },
-<<<<<<< HEAD
   titleItem: { color: textColor, fontSize: 15, fontWeight: 'bold' },
-=======
-
-  titleItem: { color: textColor, fontSize: 15, fontWeight: 'bold' },
-
->>>>>>> 620af07844c2b9366d9ea80afb831befc252bc8b
   titleView: { flex: 5, flexDirection: 'row', marginRight: '10%' }
 })
 
